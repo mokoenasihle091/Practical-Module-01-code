9e5f0880b5a23342f9e622d78fd6c7f180e8c086
Sihle <mokoenasihle091@gmail.com>
2026-04-30 17:12:39 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int choice;
+   int x =0;
+
+   do {
+    cout << "print execution" ;
+    cout << x<<"\n";
+    x++ ;
+   } while (x<5);
+
+
+
+  /* cin >> choice;
+
+   switch (choice) {
+    case 1:
+     cout<< "Order is coffee";
+     break;
+
+    case 2:
+        cout<< "Order is tea" ;
+        break;
+
+    default:
+        cout<< "Order not found" ;
+   }
+*/
+    return 0;
+}
