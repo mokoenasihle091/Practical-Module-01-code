4d4089eccd53aa9e00de944c36a47c212a590c47
Sihle <mokoenasihle091@gmail.com>
2026-05-01 20:26:34 +0200

    Added changes to file.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main()
 {
+   // Added block of code to show changes made to code after
+   // cloning the code and modifying then uploading it back
+   // to github using git
+   cout << "Line of code added to show modification made to original code"<< "\n";
+
+
    int choice;
 
    cout << "select you order: 1 For Coffee ; 2 for Tea "<<"\n";
